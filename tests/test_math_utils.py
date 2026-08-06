778d0d25fdec9161234d71b016e0ddddfc3bb40a
a-delavega <alejandro.vegzul@gmail.com>
2026-08-06 21:04:30 +0200

    test: add basic unit tests for 3D angle mathematical engine

---
+++ tests/test_math_utils.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+import sys
+import os
+
+# Add the 'src' directory to the Python path to import custom modules
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from biomechanics.math_utils import BiomechanicsMath
+
+class TestBiomechanicsMath(unittest.TestCase):
+    
+    def test_calculate_3d_angle_90_degrees(self):
+        """Test that a perfect right angle returns 90.0 degrees."""
+        p_proximal = np.array([1.0, 0.0, 0.0])  # Point on the X-axis
+        p_vertex = np.array([0.0, 0.0, 0.0])    # Vertex at the origin
+        p_distal = np.array([0.0, 1.0, 0.0])    # Point on the Y-axis
+        
+        angle = BiomechanicsMath.calculate_3d_angle(p_proximal, p_vertex, p_distal)
+        
+        # Assert that the calculated angle is exactly 90.0 degrees
+        self.assertAlmostEqual(angle, 90.0, places=2)
+
+    def test_calculate_3d_angle_180_degrees(self):
+        """Test that a straight line returns 180.0 degrees."""
+        p_proximal = np.array([-1.0, 0.0, 0.0]) # Point on the negative X-axis
+        p_vertex = np.array([0.0, 0.0, 0.0])    # Vertex at the origin
+        p_distal = np.array([1.0, 0.0, 0.0])    # Point on the positive X-axis
+        
+        angle = BiomechanicsMath.calculate_3d_angle(p_proximal, p_vertex, p_distal)
+        
+        # Assert that the calculated angle is exactly 180.0 degrees
+        self.assertAlmostEqual(angle, 180.0, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
